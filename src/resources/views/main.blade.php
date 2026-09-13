6647b6b96113037670fbaa3878044392be2d697d
m-a-t-t-h <matt@webdevops.uk>
2026-09-13 14:36:45 +0100

    [#staging] testing deployment

--- src/resources/views/main.blade.php
+++ src/resources/views/main.blade.php
@@ -3,15 +3,13 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 <title>{{ config('app.name') }}</title>
-<meta name="test" value="testing">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}"/>
-<link rel="manifest" href="/manifest.json">
+<link rel="manifest" href="manifest.json">
 @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-@vite(['resources/css/app.js', 'resources/js/app.css'])
+@vite(['resources/js/app.js', "resources/css/app.css"])
 @endif
 </head>
-
 <body><div id="app_root"></div></body>
 </html>
