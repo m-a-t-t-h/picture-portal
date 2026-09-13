8c9dc7c43014a60abdd40df253ea7789751ed96d
m-a-t-t-h <matt@webdevops.uk>
2026-09-13 11:26:08 +0100

    [#staging] testing deployment

--- src/resources/views/main.blade.php
+++ src/resources/views/main.blade.php
@@ -3,6 +3,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 <title>{{ config('app.name') }}</title>
+<meta name="test" value="testing">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}"/>
