3355e2f706663a4c4779f3caaf35a1d5717cc520
m-a-t-t-h <matt@webdevops.uk>
2026-09-16 10:12:09 +0100

    Bug fixes

--- src/app/Services/ImageFilterService.php
+++ src/app/Services/ImageFilterService.php
@@ -165,8 +165,9 @@ class ImageFilterService
                 if ($information->format === "MP3" || $information->format === "MP4") {
                     if (AuthService::isMp3Mp4DirectAccessEnabled()) {
                         $response["img_path"] =
+                            "/images/" .
                             config("dkw.IMAGE_URL_PREFIX") .
-                            "svr" . config("dkw.ROOT_COLLECTION_ID") .
+                            "col" . config("dkw.ROOT_COLLECTION_ID") .
                             str_replace(config("dkw.IMAGE_URL_PREFIX_STRIP"), "", $image->path);
                     }
                 }
